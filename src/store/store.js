3eac06399c4dc7a8a54947709d7bc7e42c25aa11
Marius-PruteanOSF <marius.prutean@osf.digital>
2022-07-02 22:53:14 +0300

    redux Persist and dev tools setting up

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
@@ -18,10 +20,26 @@ import { rootReducer } from './root-reducer';
 //   console.log('next state: ', store.getState());
 // };
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
+// or to use Redux-dev tools need to create a composeEnhancer and replace imported redux 'compose' to be able to start redux-dev tools
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
